4be3de2f70602edd51a62b971c12be583b1d68a8
Harshith <harshith.v02@gmail.com>
2024-12-06 10:11:01 +0530

    small changes

--- frontend/ems/src/app/components/daily-time-log/daily-time-log.component.ts
+++ frontend/ems/src/app/components/daily-time-log/daily-time-log.component.ts
@@ -24,7 +24,7 @@ export class DailyTimeLogComponent implements OnInit {
   constructor(
     private router: Router,
     private employeeService: EmployeeService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.initializeTimeLogs();
